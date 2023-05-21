ef7780844026bba3925ad560c960e57e3f8d3127
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-05-21 16:18:11 +0300

    Chapter 6

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'django_extensions',
+    'images.apps.ImagesConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
---
+++ bookmarks/images/views.py
@@ -0,0 +1,71 @@
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+from .forms import ImageCreateForm
+from .models import Image
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+
+            messages.success(request, 'Image added successfully')
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
+
+
+def image_detail(request, image_id, slug):
+    image = get_object_or_404(Image, id=image_id, slug=slug)
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
